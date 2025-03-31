c326b8416009c7a13739597a17d35bc7c8828fe6
Gabriel Gomes de Amorim Silva <gabrielgomes@MacBook-Air-de-Gabriel.local>
2025-03-31 09:45:58 -0300

    refactor

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://ggnoxofc.com",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
